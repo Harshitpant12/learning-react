13f53372e27189687adcf818f39812f1b2c3c7ef
Harshitpant12 <harshpant778@gmail.com>
2026-03-27 07:18:26 +0530

    add basic structure for currency converter application

---
+++ currencyconverter/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat' style={{ backgroundImage: `url(https://images.pexels.com/photos/3532540/pexels-photo-3532540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2)` }}>
+      <h1></h1>
+    </div>
+  )
+}
+
+export default App
