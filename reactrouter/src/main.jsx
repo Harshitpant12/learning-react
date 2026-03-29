a071707b69f27e15b3c025840de3df3d534ac880
Harshitpant12 <harshpant778@gmail.com>
2026-03-29 16:15:04 +0530

    add ui for User component layout, add route for user/:userId

--- reactrouter/src/main.jsx
+++ reactrouter/src/main.jsx
@@ -7,7 +7,7 @@ import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 // import Contact from './components/Contact/Contact.jsx'
 import Github, { githubInfoLoader } from './components/Github/Github.jsx'
-// import User from './components/User/User.jsx'
+import User from './components/User/User.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -19,7 +19,9 @@ const router = createBrowserRouter(
       loader={githubInfoLoader}
       path='github'
       element={<Github/>} />
-      {/* <Route path='user' element={<User/>} /> */}
+      <Route path='user/' element={<User/>} >
+        <Route path=':userId' element={<User/>} />
+      </Route>
     </Route>
   )
 )
